9e1b5f42769cbb07dd43854b8f159b3da97d15d4
JamesMcNaney <38121863+JamesMcNaney@users.noreply.github.com>
2019-06-17 16:09:36 -0500

    updating test file

--- Quadriga/tutorials/quadriga_and_mimosim.m
+++ Quadriga/tutorials/quadriga_and_mimosim.m
@@ -1,23 +1,25 @@
 % generate par parameters to be used in simpleMIMOsim and channel_sim
 
-    par.simName = 'ERR_MTxMR_16QAM'; % simulation name (used for saving results)
-    par.runId = 0; % simulation ID (used to reproduce results)
-    par.MR = 64; % receive antennas 
-    par.MT = 8; % transmit antennas (set not larger than MR!) 
-    par.mod = '16QAM'; % modulation type: 'BPSK','QPSK','16QAM','64QAM'
-    par.trials = 250; % number of Monte-Carlo trials (transmissions)
-    par.SNRdB_list = -10:2:25; % list of SNR [dB] values to be simulated
-%     par.detector = {'ZF','bMMSE','uMMSE','ML'}; % define detector(s) to be simulated  
-    par.detector = {'ZF','bMMSE','uMMSE'};
-    % Have not been able to successfully run ML
+par.simName = 'ERR_MTxMR_16QAM'; % simulation name (used for saving results)
+par.runId = 0; % simulation ID (used to reproduce results)
+par.MR = 64; % receive antennas 
+par.MT = 8; % transmit antennas (set not larger than MR!) 
+par.mod = '16QAM'; % modulation type: 'BPSK','QPSK','16QAM','64QAM'
+par.trials = 250; % number of Monte-Carlo trials (transmissions)
+par.SNRdB_list = -10:2:25; % list of SNR [dB] values to be simulated
+% par.detector = {'ZF','bMMSE','uMMSE','ML'}; % define detector(s) to be simulated  
+par.detector = {'ZF','bMMSE','uMMSE'};
+% Have not been able to successfully run ML
 
+% To be used in channel_sim.m -> where QuaDRiGa generates coefficients
 par.scenario = 'Freespace'; % 'BERLIN_UMa_NLOS', 'Freespace', 'mmMAGIC_UMi_LOS', 'mmMAGIC_UMi_NLOS'
 par.fc = 60e9; % carrier frequency [Hz]
 par.BW = 14e6; % bandwidth [Hz]
 par.N = 2048; % number of carriers
-par.B = 64; % number of antennas in the BS (we use a single BS)
-par.U = 8; % number of single-antenna UEs
-par.iid = 1;
+par.B = par.MR; % number of antennas in the BS (we use a single BS)
+par.U = par.MT; % number of single-antenna UEs
+par.iid = 1;    % simulates Gaussian iid
+
 % Run the simulation
 
 subplot(1,2,1)
@@ -25,7 +27,7 @@ simpleMIMOsim(par);
 title('Gaussian iid channel');
 
 subplot(1,2,2)
-par.iid = 0;
+par.iid = 0;    % simulates the par.scenario
 simpleMIMOsim(par);
 title('QuaDRiGa Freespace Channel');
 
